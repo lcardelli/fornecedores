2d5481b2b759d66c7d98a1d40af458416856991d
lcardelli <lcardelli@notredamecampinas.net.br>
2024-09-24 17:26:57 -0300

    add routes

--- router/routes.go
+++ router/routes.go
@@ -2,17 +2,40 @@ package router
 
 import "github.com/gin-gonic/gin"
 
-// InitializeRoutes initializes the routes for the application	
+// InitializeRoutes initializes the routes for the application
 func InitializeRoutes(router *gin.Engine) {
 	// Create a new group for the v1 API
 	v1 := router.Group("/api/v1")
 	{
 		// Create a new group for the suppliers
-		v1.GET("suppliers", func(ctx *gin.Context) {
+		v1.GET("/supplie", func(ctx *gin.Context) {
 			ctx.JSON(200, gin.H{
-				"message": "Hello, World!",
+				"msg": "Suppliers",
+			})
+		})
+
+		v1.POST("/suppliers", func(ctx *gin.Context) {
+			ctx.JSON(200, gin.H{
+				"msg": "Suppliers",
+			})
+		})
+
+		v1.DELETE("/suppliers", func(ctx *gin.Context) {
+			ctx.JSON(200, gin.H{
+				"msg": "Suppliers",
+			})
+		})
+
+		v1.PUT("/suppliers", func(ctx *gin.Context) {
+			ctx.JSON(200, gin.H{
+				"msg": "Suppliers",
+			})
+		})
+
+		v1.PUT("/suppliers", func(ctx *gin.Context) {
+			ctx.JSON(200, gin.H{
+				"msg": "Suppliers",
 			})
 		})
 	}
 }
-
