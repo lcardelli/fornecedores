e6a0b29520ee443073e855337163ac01b9fee0a5
lcardelli <lcardelli@notredamecampinas.net.br>
2024-10-09 18:41:56 -0300

    feat: table with count of suppliers to category

--- handler/dashboard.go
+++ handler/dashboard.go
@@ -8,10 +8,9 @@ import (
 	"html/template"
 )
 
-// DashboardHandler renderiza o template do dashboard com as informações do usuário
 func DashboardHandler(c *gin.Context) {
-	session := sessions.Default(c)  
-	userID := session.Get("userID") // Obtém o ID do usuário da sessão 
+	session := sessions.Default(c)
+	userID := session.Get("userID") // Obtém o ID do usuário da sessão
 
 	if userID == nil {
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
@@ -24,9 +23,37 @@ func DashboardHandler(c *gin.Context) {
 		return
 	}
 
+	supplierCountByCategory, err := GetSupplierCountByCategory()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao buscar dados"})
+		return
+	}
+
 	// Carregar templates
 	tmpl := template.Must(template.ParseGlob("templates/*"))
 	tmpl.ExecuteTemplate(c.Writer, "dashboard.html", gin.H{
-		"user": user,
+		"user":                    user,
+		"supplierCountByCategory": supplierCountByCategory,
 	})
+}
+
+// GetSupplierCountByCategory retorna a contagem de fornecedores por categoria
+func GetSupplierCountByCategory() ([]struct { 	
+	CategoryName string `json:"category_name"`
+	Count        int    `json:"count"`
+}, error) {
+	var results []struct {
+		CategoryName string `json:"category_name"`
+		Count        int    `json:"count"`
+	}
+
+	if err := db.Table("suppliers").
+		Select("supplier_categories.name as category_name, COUNT(suppliers.id) as count").
+		Joins("LEFT JOIN supplier_categories ON suppliers.category_id = supplier_categories.id").
+		Group("supplier_categories.name").
+		Scan(&results).Error; err != nil {
+		return nil, err 
+	}
+
+	return results, nil
 }
